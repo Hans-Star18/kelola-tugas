69b3354adbb8bc25b32050fdfe1c421ce24318c7
Hans-Star18 <agussuardiasa1231@gmail.com>
2022-05-19 15:24:56 +0800

    menambahkan view dari setor tugas

--- app/Helpers/MyHelpers.php
+++ app/Helpers/MyHelpers.php
@@ -13,7 +13,7 @@ class MyHelpers
 
     public static function tasks()
     {
-        DB::table('tasks')
+        return DB::table('tasks')
             ->join('mata_pelajaran', 'tasks.mata_pelajaran_id', '=', 'mata_pelajaran.id')
             ->join('statuses', 'tasks.status_id', '=', 'statuses.id')
             ->select('tasks.id', 'tasks.status_id', 'tasks.judul_tugas', 'tasks.deskripsi_tugas', 'tasks.deadline_at', 'tasks.tanggal_dibuat', 'tasks.tanggal_dikumpul', 'mata_pelajaran.mata_pelajaran', 'statuses.status_name');
