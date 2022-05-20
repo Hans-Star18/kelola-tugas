803e7c4cb65bdd42490458677e6051b9a3b43a8e
Hans-Star18 <agussuardiasa1231@gmail.com>
2022-05-20 20:20:32 +0800

    menambahakan modal kumpulkan tugas dan tambah text editor

--- resources/views/tugas/setor_tugas.blade.php
+++ resources/views/tugas/setor_tugas.blade.php
@@ -7,18 +7,21 @@
                 <div class="col-lg-3 col-md-4 mb-3">
                     <div
                         class="card text-center border {{ MyHelpers::strtodate($task->deadline_at)->diff()->invert == 1 ? 'border-warning' : 'border-danger' }}">
-                        <div class="card-header">{{ $task->status_name }}</div>
+                        <div class="card-header">
+                            {{ MyHelpers::strtodate($task->deadline_at)->diff()->invert == 0 ? 'Lewat Waktu' : 'Belum Selesai' }}
+                        </div>
                         <div class="card-body">
                             <h5 class="card-title">{{ $task->mata_pelajaran }}</h5>
                             <p class="card-text">{{ $task->judul_tugas }}</p>
-                            <a href="javascript:void(0)" class="btn btn-primary">Kumpulkan</a>
+                            <a href="javascript:void(0)" class="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#modalCenter">Kumpulkan</a>
                         </div>
                         <div class="card-footer text-muted">{{ MyHelpers::strtodate($task->deadline_at)->diffForHumans() }}
                         </div>
                     </div>
                 </div>
             @endforeach
-
         </div>
     </div>
 @endsection
+@include('tugas.partials.modals')
